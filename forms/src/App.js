ec26c70b622ff684195cc82ecfb119c3bf3f5a9e
DanielBarbakadze <danielbarbakadze@gmail.com>
2019-10-28 17:16:46 +0400

    working on login form

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleFieldChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render (){
+    return(
+    <div>
+      <div>
+        <label>Username</label>
+        <Input
+          name='username'
+          value={this.state.username}
+          onChange={(e) => this.handleFieldChange(e)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <Input 
+          name='password'
+          value={this.state.password}
+          type='password' 
+          onChange={(e) => this.handleFieldChange(e)}
+        />
+      </div>
+      <div>
+        <button onClick={() => console.log(this.state.username,this.state.password)}>
+          Submit
+        </button>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default App; 
---
+++ forms/src/components/Input.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render () {
+        const {
+            name,
+            onChange,
+            value,
+            type='text'
+        } = this.props;
+
+        return (
+            <input
+                name={name}
+                onChange={onChange}
+                value={value}
+                type={type}
+            />
+        )
+    }
+}
+export default Input
